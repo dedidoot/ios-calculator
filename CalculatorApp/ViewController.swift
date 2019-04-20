22ab8a2924e1aa516294e65f10b9763d92120627
Dedi Dot <hello.dedidot@gmail.com>
2019-04-20 20:59:24 +0700

    Initial Commit

---
+++ CalculatorApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalculatorApp
+//
+//  Created by Dedi Dot on 20/04/19.
+//  Copyright © 2019 Dedi Dot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
